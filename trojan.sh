9a58175f52e1a9bb2af856703493f55e1611f6cf
pushy <80036867+dahlah270@users.noreply.github.com>
2022-03-21 19:51:33 +0700

    Create trojan.sh

---
+++ trojan.sh
@@ -0,0 +1,196 @@
+# Halahh Kang Recode Aja
+# Bilang Ke Authornya Nyett
+#!/bin/category/bash
+
+gn='\033[0;32m'
+gl='\033[32;1m'
+rd='\033[31;1m'
+we='\033[37;1m'
+bk='\033[30;1m'
+yw='\033[33;1m'
+yl='\033[1;33m'
+be='\033[36;1m'
+bl='\033[0;36m'
+
+# subrek
+xdg-open "https://youtube.com/channel/UCbbDomE4Dj1_ghX2VSrPuYg"
+echo -e $gl "Subrek"
+# tampilan
+clear
+echo -e $rd "██╗  ██╗ ██████╗ ███╗   ██╗████████╗ █████╗ ██╗  ██╗"
+echo -e $rd "██║ ██╔╝██╔═══██╗████╗  ██║╚══██╔══╝██╔══██╗██║ ██╔╝"
+echo -e $rd "█████╔╝ ██║   ██║██╔██╗ ██║   ██║   ███████║█████╔╝" 
+echo -e $we "██╔═██╗ ██║   ██║██║╚██╗██║   ██║   ██╔══██║██╔═██╗ "
+echo -e $we "██║  ██╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║  ██╗"
+echo -e $we "╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝"
+echo -e
+echo -e
+echo -e
+echo -e $we "∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+echo -e $rd "1]. Cari Kontak"
+echo -e $rd "2]. Keluar"
+echo -e $we "∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+  echo -e $bl "Pilih Ngabb??!!"
+      read -p "=>" lp
+if [[ $lp == 1 || $lp == 1 ]]
+then
+clear
+echo -e $rd "Tungguu Yaa =======-------------------------------------------"
+echo -e $we "10%"
+sleep 6
+clear
+echo -e $rd "Tungguu Yaa =============-------------------------------------"
+echo -e $we "30%"
+sleep 6
+clear
+echo -e $rd "Tungguu Yaa =========================-------------------------"
+echo -e $we "50%"
+sleep 6
+clear
+echo -e $rd "Tungguu Yaa ====================================---------------"
+echo -e $we "78%"
+sleep 6
+clear
+echo -e $rd "Tungguu Yaa ==================================================-"
+echo -e $rd "Dikit Lagi Yaa..."
+echo -e $we "99%"
+sleep 6
+clear
+echo -e $bl "Wahh Udah Selesai, Saatnya Memulaikan"
+sleep 2
+echo -e $rd "Sepertinya Kau Belum Menginstall Bahannya"
+sleep 0.1
+echo -e $we "Saatnya Menginstall"
+pip2 install requests
+pkg install python2
+pip2 install mechanize
+pip3 install bs4
+sleep 2
+echo -e $yl "Sudah Selesai Menginstall nya"
+echo -e $bl "Sedang Ke Target"
+sleep 4
+clear
+echo -e $rd "██╗  ██╗ ██████╗ ███╗   ██╗████████╗ █████╗ ██╗  ██╗"
+echo -e $rd "██║ ██╔╝██╔═══██╗████╗  ██║╚══██╔══╝██╔══██╗██║ ██╔╝"
+echo -e $rd "█████╔╝ ██║   ██║██╔██╗ ██║   ██║   ███████║█████╔╝ "
+echo -e $we "██╔═██╗ ██║   ██║██║╚██╗██║   ██║   ██╔══██║██╔═██╗ "
+echo -e $we "██║  ██╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║  ██╗"
+echo -e $we "╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝"
+echo -e
+echo -e
+echo -e $bk "∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+echo -e $rd "1]. Hack Sekarang"
+echo -e $bk "∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+      echo -e $we "Pilih"
+         read -p "=>" Pl
+fi
+if [[ $Pl == 1 || $Pl == 1 ]]
+then
+clear
+      read -p "Kasih Target =>" op
+clear
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2                                                 
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2                                                 
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2                                                 
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2                                                 
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $rd "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2
+echo -e $we "Sedang Mengirim Virus TR0J4NS Ke Nomor $op"
+sleep 2                                                 
+clear
+echo -e $rd "Sudah Mengirim Virus"
+echo -e $we "Sedang Di Kembalikan"
+sleep 0.1
+bash OntakHack.sh
+fi
+if [[ $lp == 2 || $lp == 2 ]]
+then
+clear
+echo -e $rd "██████╗  █████╗ ██████╗  █████╗ ██╗  ██╗"
+echo -e $rd "██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║  ██║"
+echo -e $rd "██║  ██║███████║██║  ██║███████║███████║"
+echo -e $we "██║  ██║██╔══██║██║  ██║██╔══██║██╔══██║"
+echo -e $we "██████╔╝██║  ██║██████╔╝██║  ██║██║  ██║"
+echo -e $we "╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝"
+exit                                      
+fi
